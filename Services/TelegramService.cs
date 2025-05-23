ba57b2ecfc493bf9ad223b5189cdf6080323a501
Razglidai <53232884+Razglidai@users.noreply.github.com>
2025-05-23 20:48:53 +0300

    хж

--- Services/TelegramService.cs
+++ Services/TelegramService.cs
@@ -6,10 +6,22 @@ public class TelegramService
 {
     private readonly IConfiguration _configuration;
     private readonly TelegramBotClient _botClient;
+    private readonly string? _chatId;
     public TelegramService(IConfiguration configuration)
     {
         _configuration = configuration;
+        var token = _configuration
+            .GetSection("TelegramBot")
+            .GetValue<string>("ApiKey");
+        _botClient = new TelegramBotClient(token);
+        _chatId = _configuration
+            .GetSection("TelegramBot")
+            .GetValue<string>("ChatId");
     }
-    
+
+    public void Send(string text)
+    {
+        _botClient.SendMessage(_chatId, text);
+    }
     
 }
