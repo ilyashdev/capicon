c4e93cbc9a513df1f8a56905629664ec0779c61b
turboskomorokh <turboskomorokh@gmail.com>
2025-05-23 19:47:50 +0300

    я хочу

--- Services/PostService.cs
+++ Services/PostService.cs
@@ -4,30 +4,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace capicon.Services;
 
-public class PostService
+public class PostService(CSDbContext context)
 {
-    private readonly CSDbContext _context;
     private readonly int DIFF = 8;
-
-    public PostService(CSDbContext context)
-    {
-        _context = context;
-    }
 
     public async Task AddPostAsync(PostModel model)
     {
-        await _context.News.AddAsync(model);
-        await _context.SaveChangesAsync();
+        await context.News.AddAsync(model);
+        await context.SaveChangesAsync();
     }
     public async Task<PostModel?> GetPostAsync(int? id)
     {
         if (id == null) return null;
-        return await _context.News.FirstOrDefaultAsync(n => n.Id == id.Value);
+        return await context.News.FirstOrDefaultAsync(n => n.Id == id.Value);
     }
 
     public async Task<List<PostModel>?> GetPostsAsync(int idOffset = 0)
     {
-        return await _context.News
+        return await context.News
             .OrderBy(n => n.dateTime)
             .Skip(idOffset * DIFF)
             .Take(DIFF)
@@ -36,9 +30,9 @@ public class PostService
 
     public async Task DeletePostAsync(int id)
     {
-        var post = await _context.News.FirstAsync(n => n.Id == id);
-        _context.News.Remove(post);
-        await _context.SaveChangesAsync();
+        var post = await context.News.FirstAsync(n => n.Id == id);
+        context.News.Remove(post);
+        await context.SaveChangesAsync();
     }
 
     
