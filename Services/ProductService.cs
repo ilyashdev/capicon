bcc50910f7ba0f9e6ab22ca41cac7d53d3b2251a
Razglidai <53232884+Razglidai@users.noreply.github.com>
2025-05-23 19:31:24 +0300

    хуй

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,7 +1,7 @@
 using capicon.Models;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
-
+using capicon.Settings;
 public class ProductService
 {
     private readonly CSDbContext _context;
@@ -11,28 +11,30 @@ public class ProductService
         _context = context;
     }
 
-    public async Task CreateProduct(ProductViewModel product)
+    public async Task CreateProduct(ProductViewModel? product)
     {
         _context.Products.Add(product);
         await _context.SaveChangesAsync();
     }
 
-    public async Task<List<ProductViewModel>> SearchProducts(string query) =>
+    public async Task<List<ProductViewModel>> SearchProducts(string query, int page) =>
       await _context.Products
         .Include(p => p.Specifications)
         .Include(p => p.Details)
         .Where(p => p.Title.Contains(query) || 
                     p.Description1.Contains(query) || 
                     p.Description2.Contains(query))
+        .Skip(page * PageSettings.PAGE_SIZE)
+        .Take(PageSettings.PAGE_SIZE)
         .ToListAsync();
 
-    public async Task<ProductViewModel> GetProduct(int id) =>
+    public async Task<ProductViewModel?> GetProduct(int id) =>
       await _context.Products
         .Include(p => p.Specifications)
         .Include(p => p.Details)
         .FirstOrDefaultAsync(p => p.Id == id);
 
-    public async Task UpdateProduct(ProductViewModel product)
+    public async Task UpdateProduct(ProductViewModel? product)
     {
         _context.Products.Update(product);
         await _context.SaveChangesAsync();
