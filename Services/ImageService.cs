d78809acf138c0ce4a698b397229a923c98f87e7
Razglidai <53232884+Razglidai@users.noreply.github.com>
2025-06-01 13:45:55 +0300

    image add

---
+++ Services/ImageService.cs
@@ -0,0 +1,22 @@
+﻿namespace capicon.Services;
+
+public class ImageService
+{
+    private readonly IWebHostEnvironment _env;
+
+    public ImageService(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    public string SaveImage(IFormFile file)
+    {
+        if (!file.ContentType.Contains("image"))
+            throw new Exception("invalid file type");
+        using (var fileStream = new FileStream(Path.Combine(_env.WebRootPath, "image", file.FileName), FileMode.Create))
+        {
+            file.CopyTo(fileStream);
+        }
+        return Path.Combine("image", file.FileName);
+    }
+}
